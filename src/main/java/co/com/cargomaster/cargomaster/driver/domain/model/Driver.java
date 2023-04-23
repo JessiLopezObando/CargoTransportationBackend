02bece1deca0b85983f306d84a046477f7bad936
William Serna <williamsernac@hotmail.com>
2023-04-23 17:45:25 -0500

    Created Driver model and gateway

---
+++ src/main/java/co/com/cargomaster/cargomaster/driver/domain/model/Driver.java
@@ -0,0 +1,24 @@
+package co.com.cargomaster.cargomaster.driver.domain.model;
+
+import co.com.cargomaster.cargomaster.driver.domain.model.ValueObjects.Vehicle;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Driver {
+
+    private String id;
+    private String username;
+    private String name;
+    private String lastName;
+    private String dni;
+    private String email;
+    private String phone;
+    private Integer age;
+    private Vehicle vehicle;
+
+
+}
---
+++ src/main/java/co/com/cargomaster/cargomaster/driver/domain/model/gateway/DriversGateway.java
@@ -0,0 +1,14 @@
+package co.com.cargomaster.cargomaster.driver.domain.model.gateway;
+
+import co.com.cargomaster.cargomaster.driver.domain.model.Driver;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface DriversGateway {
+
+    Flux<Driver> getAllDrivers();
+    Mono<Driver> getDriverById(String id);
+    Mono<Driver> saveDriver(Driver driver);
+    Mono<String> deleteDriver(String id);
+    Mono<Driver> updateDriver(String id, Driver driver);
+}

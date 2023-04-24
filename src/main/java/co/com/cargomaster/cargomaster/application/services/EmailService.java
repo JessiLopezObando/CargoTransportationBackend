83adef83728e17776e45b8963d48d68c083c4cde
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-04-24 12:32:50 -0500

    Added config of Email Sender such as the service to send the email

---
+++ src/main/java/co/com/cargomaster/cargomaster/application/services/EmailService.java
@@ -0,0 +1,43 @@
+package co.com.cargomaster.cargomaster.application.services;
+
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.core.io.InputStreamSource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void send(String from, String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
+
+    public void sendWithAttach(String from, String to, String subject,
+                               String text, String attachName,
+                               InputStreamSource inputStream) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text, true);
+        helper.addAttachment(attachName, inputStream);
+        mailSender.send(message);
+    }
+
+}

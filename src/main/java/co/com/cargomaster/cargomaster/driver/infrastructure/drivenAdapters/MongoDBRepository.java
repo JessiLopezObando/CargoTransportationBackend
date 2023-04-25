0379c8f19c409808bdfcaf8007282363286e37b2
William Serna <williamsernac@hotmail.com>
2023-04-25 13:27:21 -0500

    Added necessary queries to Repository to perform validations

--- src/main/java/co/com/cargomaster/cargomaster/driver/infrastructure/drivenAdapters/MongoDBRepository.java
+++ src/main/java/co/com/cargomaster/cargomaster/driver/infrastructure/drivenAdapters/MongoDBRepository.java
@@ -3,9 +3,15 @@ package co.com.cargomaster.cargomaster.driver.infrastructure.drivenAdapters;
 import co.com.cargomaster.cargomaster.driver.domain.model.Driver;
 import co.com.cargomaster.cargomaster.driver.infrastructure.drivenAdapters.data.DriverData;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface MongoDBRepository extends ReactiveMongoRepository<DriverData, String> {
 
     Mono<Driver> findDriverByEmail(String email);
+
+    Mono<Driver> findDriverByDni(String dni);
+    Flux<Driver> findDriverByUsername(String username);
+
+
 }
